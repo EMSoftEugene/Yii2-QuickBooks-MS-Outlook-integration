77533dc832ee96ec48baa046ed3d51ef9f410bfb
Evgeny Monsvetov <emonsvetov@incento.com>
2024-10-22 19:34:56 +0300

    fix

--- intempus/controllers/MicrosoftController.php
+++ intempus/controllers/MicrosoftController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use app\models\MicrosoftEvent;
 use app\models\User;
+use app\services\MicrosoftService;
+use Microsoft\Graph\Generated\Groups\GroupsRequestBuilderGetRequestConfiguration;
 use Microsoft\Graph\Generated\Groups\Item\Events\EventsRequestBuilderGetRequestConfiguration;
 use Microsoft\Graph\GraphServiceClient;
 use yii\filters\AccessControl;
@@ -15,9 +17,15 @@ use Microsoft\Kiota\Authentication\Oauth\ProviderFactory;
 use Microsoft\Graph\Core\Requests\BatchRequestContent;
 use Microsoft\Graph\BatchRequestBuilder;
 use Microsoft\Graph\Core\Requests\BatchResponseItem;
+use app\services\interfaces\MicrosoftInterface;
 
 class MicrosoftController extends Controller
 {
+    public array $groupsEventScopes = [
+        'User.Read',
+        'Group.Read.All',
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -53,13 +61,14 @@ class MicrosoftController extends Controller
             $_ENV['REDIRECT_URI'],
         );
 
+        $scope = 'offline_access User.Read Group.Read.All';
         $scope = 'User.Read Group.Read.All';
         $provider = ProviderFactory::create($tokenRequestContext);
         $authUrl = $provider->getAuthorizationUrl();
-        $authUrl.= '&redirect_uri='.urlencode($_ENV['REDIRECT_URI']);
-        $authUrl.= '&response_mode=query';
-        $authUrl.= '&client_id='.$_ENV['CLIENT_ID'];
-        $authUrl.= '&scope='.$scope;
+        $authUrl .= '&redirect_uri=' . urlencode($_ENV['REDIRECT_URI']);
+        $authUrl .= '&response_mode=query';
+        $authUrl .= '&client_id=' . $_ENV['CLIENT_ID'];
+        $authUrl .= '&scope=' . urlencode($scope);
         return $this->redirect($authUrl);
     }
 
@@ -74,17 +83,118 @@ class MicrosoftController extends Controller
         $code = \Yii::$app->request->get('code');
 
         $user = User::findOne(['is_admin' => 1]);
-        if($user){
+        if ($user) {
             $user->microsoft_auth = $code;
             $user->save();
             \Yii::$app->session->setFlash('Token updated');
         }
+
+        $tokenRequestContext = new AuthorizationCodeContext(
+            $_ENV['TENANT_ID'],
+            $_ENV['CLIENT_ID'],
+            $_ENV['CLIENT_SECRET'],
+            $code,
+            $_ENV['REDIRECT_URI'],
+        );
+        $graphClient = new GraphServiceClient($tokenRequestContext, $this->groupsEventScopes);
+
+        $requestConfigurationGroup = new GroupsRequestBuilderGetRequestConfiguration();
+        $queryParameters = GroupsRequestBuilderGetRequestConfiguration::createQueryParameters();
+        $queryParameters->top = 500;
+        $requestConfigurationGroup->queryParameters = $queryParameters;
+
+        $groupIds = [];
+        $groups = $graphClient->groups()->get($requestConfigurationGroup)->wait();
+        foreach ($groups->getValue() as $group) {
+            $groupIds[] = $group->getId();
+        }
+
+
+        $result = [];
+        $format = 'Y-m-d H:i:s';
+        $date = new \DateTime();
+        $date->modify('first day of this month');
+        $startDate = $date->format($format);
+
+        $date->add(new \DateInterval('P30D'));
+        $endDate = $date->format($format);
+
+        $requestConfiguration = new EventsRequestBuilderGetRequestConfiguration();
+        $queryParameters = EventsRequestBuilderGetRequestConfiguration::createQueryParameters();
+        $queryParameters->orderby = ["start/dateTime"];
+        $queryParameters->select = ["subject", "locations", "start"];
+        $queryParameters->top = 100;
+//        $queryParameters->filter = "start/dateTime gt '$startDate' and start/dateTime lt '$endDate'";
+        $queryParameters->filter = "start/dateTime gt '$startDate'";
+        $requestConfiguration->queryParameters = $queryParameters;
+
+        foreach ($groupIds as $groupId) {
+            try {
+                $events = $graphClient->groups()
+                    ->byGroupId($groupId)
+                    ->events()
+                    ->get($requestConfiguration)->wait();
+            } catch (\Exception $e) {
+                continue;
+            }
+
+            foreach ($events->getValue() as $event) {
+                $eventId = $event->getId();
+                $eventSubject = $event->getSubject();
+                $eventTime = $event->getStart()->getDateTime();
+                $tmpLocations = $event->getLocations();
+
+                $location = '';
+                foreach ($tmpLocations as $location) {
+                    $locationString = $location->getDisplayName();
+                    if ($locationString) {
+                        $location = $locationString;
+                    }
+                }
+                $result[] = [
+                    'eventSubject' => $eventSubject,
+                    'eventStartTime' => $eventTime,
+                    'location' => $location,
+                ];
+            }
+        }
+
+        foreach ($result as $event) {
+            $eventLocation = $event['location'] ?? null;
+            $existingEvent = MicrosoftEvent::findOne(['location' => $eventLocation]);
+            if ($existingEvent) {
+                continue;
+            }
+            $microsoftEvent = new MicrosoftEvent();
+            $microsoftEvent->subject = $event['eventSubject'] ?? '';
+            $microsoftEvent->eventStartTime = $event['eventStartTime'] ?? '';
+            $microsoftEvent->location = $event['location'] ?? '';
+            $microsoftEvent->save();
+        }
+
+        echo "!Ok";
+        die;
+
+        //////////////////////////
+
+
+        $microsoftService = new \app\services\MicrosoftService();
+
+        $groups = $microsoftService->getGroups($user);
+        foreach ($groups as $groupId) {
+            $result = $microsoftService->getEventsByGroupId($user, $groupId);
+            $microsoftService->saveEvents($result);
+        }
+
+        echo "!Ok";
+        die;
+
         return $this->redirect('/');
     }
 
     public function actionLocations()
     {
-        $events = MicrosoftEvent::find()->select(['id', 'subject','eventStartTime','location'])->asArray()->all();
+        $events = MicrosoftEvent::find()->select(['id', 'subject', 'eventStartTime', 'location'])->asArray()->all();
         echo "Locations: <pre>";
         print_r($events);
         die;
