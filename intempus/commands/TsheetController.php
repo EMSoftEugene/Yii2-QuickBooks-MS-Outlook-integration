6425b1e7013ae2c9d39525462220ddeb3febe0a7
Evgeny Monsvetov <emonsvetov@incento.com>
2024-10-24 22:30:17 +0300

    fix

--- intempus/commands/TsheetController.php
+++ intempus/commands/TsheetController.php
@@ -16,7 +16,7 @@ class TsheetController extends Controller
         parent::__construct($id, $module, $config);
     }
 
-    public function actionAdd()
+    public function actionIndex()
     {
         $user = User::findOne(['is_admin' => 1]);
         $tsheetConfig = \Yii::$app->params['tsheet'];
@@ -27,11 +27,53 @@ class TsheetController extends Controller
             'Authorization' => 'Bearer ' . $user->tsheets_access_token,
             'Accept' => 'application/json',
         ];
+
+        $queryParams = [
+        ];
         $response = $client->request('GET', 'locations', [
             'headers' => $headers,
+            'query' => $queryParams
         ]);
 
-        print_r($response->getBody());
+        $result = $response->getBody()->getContents();
+        $result = json_decode($result, true);
+
+        print_r($result);
+        die;
+
+
+    }
+
+    public function actionAdd()
+    {
+        $user = User::findOne(['is_admin' => 1]);
+        $baseUri = 'https://rest.tsheets.com/api/v1/';
+        $client = new Client(['base_uri' => $baseUri]);
+        $headers = [
+            'Authorization' => 'Bearer ' . $user->tsheets_access_token,
+            'Accept' => 'application/json',
+        ];
+
+        $params = [];
+        $params['data'] = [];
+        $params['data'][] = (object)[
+            'addr1' => '1170 FOSTER CITY Blvd #302, Foster City, CA, 94404',
+            'addr2' => '',
+            'city' => 'FOSTER CITY',
+            'state' => 'CA',
+            'zip' => '94404',
+            'country' => 'USA',
+        ];
+
+        $response = $client->request('POST', 'locations', [
+            'headers' => $headers,
+            'form_params' => $params
+        ]);
+
+        $result = $response->getBody()->getContents();
+        $result = json_decode($result, true);
+
+        print_r($result);
         die;
 
 
